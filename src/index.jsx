d5bb494ba1f3352c6afe6f571f2d2b3ca291006f
Kasper_Chen <neko12377@gmail.com>
2020-02-29 01:04:21 +0800

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import styles from './index.scss';
+import styles from './index.scss'
+import { NavigationBar } from './JSX/navigation.jsx';
 
 function Capoo() {
   return (
-    <ul>
-      [
-      <li>443</li>
-      ,
-      <li>4123</li>
-      ,
-      <li>998</li>
-      ,
-      ]
-    </ul>
+    <div>
+      <NavigationBar />
+      
+    </div>
   );
 }
 
